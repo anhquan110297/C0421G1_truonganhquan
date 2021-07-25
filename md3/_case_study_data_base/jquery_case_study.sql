a6e2b65054da3a338a275000db48fed3d5e01f82
unknown <taqdnc@gmail.com>
2021-07-25 15:46:21 +0700

    up

--- md3/_case_study_data_base/jquery_case_study.sql
+++ md3/_case_study_data_base/jquery_case_study.sql
@@ -104,6 +104,94 @@ from khach_hang kh
 WHERE year(hd.ngay_lam_hop_dong) = 2019
 group by month(hd.ngay_lam_hop_dong);
 
+-- 10.	Hiển thị thông tin tương ứng với từng Hợp đồng thì đã sử dụng bao nhiêu Dịch vụ đi kèm. 
+-- Kết quả hiển thị bao gồm IDHopDong, NgayLamHopDong, NgayKetthuc, TienDatCoc, SoLuongDichVuDiKem 
+-- (được tính dựa trên việc count các IDHopDongChiTiet).
+
+SELECT hd.id_hop_dong,hd.ngay_lam_hop_dong,hd.ngay_ket_thuc_hop_dong,hd.tien_dat_coc,count(hdct.id_dich_vu_di_kem) as so_luong_dich_vu_di_kem
+from hop_dong hd
+	inner join hop_dong_chi_tiet hdct on hd.id_hop_dong = hdct.id_hop_dong
+    inner join dich_vu_di_kem dvdk on hdct.id_dich_vu_di_kem = dvdk.id_dich_vu_di_kem
+GROUP BY hd.id_hop_dong;
+
+-- 11.	Hiển thị thông tin các Dịch vụ đi kèm đã được sử dụng 
+-- bởi những Khách hàng có TenLoaiKhachHang 
+-- là “Diamond” và có địa chỉ là “Vinh” hoặc “Quảng Ngãi”.
+
+select dvdk.id_dich_vu_di_kem,dvdk.ten_dich_vu_di_kem,kh.id_khach_hang,kh.ten_khach_hang,kh.id_loai_khach,lk.ten_loai_khach,kh.dia_chi_khach_hang
+from khach_hang kh
+	inner join loai_khach lk on kh.id_loai_khach = lk.id_loai_khach
+    inner join hop_dong hd on kh.id_khach_hang = hd.id_khach_hang
+    inner join hop_dong_chi_tiet hdct on hd.id_hop_dong = hdct.id_hop_dong
+    inner join dich_vu_di_kem dvdk on hdct.id_dich_vu_di_kem = dvdk.id_dich_vu_di_kem
+where kh.dia_chi_khach_hang in (
+	select dia_chi_khach_hang
+    from khach_hang
+    where dia_chi_khach_hang = 'Vinh' or dia_chi_khach_hang = 'Quảng Trị'
+) and kh.id_khach_hang in (
+	select id_loai_khach
+    from khach_hang
+    where id_loai_khach = 1
+);
+
+-- 12.	Hiển thị thông tin IDHopDong, TenNhanVien, TenKhachHang, SoDienThoaiKhachHang, TenDichVu, SoLuongDichVuDikem 
+-- (được tính dựa trên tổng Hợp đồng chi tiết), TienDatCoc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2019 
+-- nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2019.
+
+select hd.id_hop_dong,nv.ten_nhan_vien,kh.ten_khach_hang,kh.so_dien_thoai,dv.ten_dich_vu,count(hdct.id_dich_vu_di_kem) as so_luong_dich_vu_di_kem,hd.tien_dat_coc
+from khach_hang kh
+	right join hop_dong hd on kh.id_khach_hang = hd.id_khach_hang
+    right join nhan_vien nv on hd.id_nhan_vien = nv.id_nhan_vien
+    right join dich_vu dv on dv.id_dich_vu = hd.id_dich_vu
+    right join hop_dong_chi_tiet hdct on hd.id_hop_dong = hdct.id_hop_dong
+    right join dich_vu_di_kem dvdk on hdct.id_dich_vu_di_kem = dvdk.id_dich_vu_di_kem
+where EXISTS (
+	SELECT tien_dat_coc
+    from hop_dong
+    where  month(ngay_lam_hop_dong) in (10,11,12)
+) and not EXISTS (
+	SELECT tien_dat_coc
+    from hop_dong
+    where  month(ngay_lam_hop_dong) in (1,2,3,4,6,7,8,9)
+)
+GROUP BY hd.id_hop_dong;
+
+-- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
+-- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+select dvdk.id_dich_vu_di_kem,dvdk.ten_dich_vu_di_kem, count(hdct.id_dich_vu_di_kem) as dich_vu_di_kem_nhieu_nhat
+from khach_hang kh
+	inner join hop_dong hd on kh.id_khach_hang = hd.id_hop_dong
+    inner join hop_dong_chi_tiet hdct on hd.id_hop_dong = hdct.id_hop_dong
+    inner join dich_vu_di_kem dvdk on hdct.id_dich_vu_di_kem = dvdk.id_dich_vu_di_kem
+GROUP BY dvdk.ten_dich_vu_di_kem;
+
+-- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm 
+-- chỉ mới được sử dụng một lần duy nhất. 
+-- Thông tin hiển thị bao gồm IDHopDong, TenLoaiDichVu, TenDichVuDiKem, SoLanSuDung.
+
+select hd.id_hop_dong,ldv.ten_loai_dich_vu,dvdk.ten_dich_vu_di_kem, count(hdct.id_dich_vu_di_kem) as dich_vu_di_kem_nhieu_nhat
+from khach_hang kh
+	inner join hop_dong hd on kh.id_khach_hang = hd.id_hop_dong
+    inner join hop_dong_chi_tiet hdct on hd.id_hop_dong = hdct.id_hop_dong
+    inner join dich_vu_di_kem dvdk on hdct.id_dich_vu_di_kem = dvdk.id_dich_vu_di_kem
+    inner join dich_vu dv on hd.id_dich_vu = dv.id_dich_vu
+    inner join loai_dich_vu ldv on dv.id_loai_dich_vu = ldv.id_loai_dich_vu
+GROUP BY dvdk.ten_dich_vu_di_kem
+having count(hdct.id_dich_vu_di_kem) = 1;
+
+15.	Hiển thi thông tin của tất cả nhân viên bao gồm IDNhanVien, HoTen, TrinhDo, TenBoPhan, SoDienThoai, DiaChi
+ mới chỉ lập được tối đa 3 hợp đồng từ năm 2018 đến 2019.
+
+
+
+
+    
+
+    
+   
+    
+
+
 
 
 
